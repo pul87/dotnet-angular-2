e33f38ca48e33da3c793fab5ed577db01dc40834
Paolo Basso <paolo.basso87@gmail.com>
2017-07-29 15:13:17 +0200

    Creazione Feature

---
+++ Migrations/20170729075958_SeedFeatures.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Vega.Migrations
+{
+    public partial class SeedFeatures : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("INSERT INTO Features (Name) VALUES ('Feature 1')");
+            migrationBuilder.Sql("INSERT INTO Features (Name) VALUES ('Feature 2')");
+            migrationBuilder.Sql("INSERT INTO Features (Name) VALUES ('Feature 3')");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("DELETE FROM Features WHERE Name in ('Feature 1', 'Feature 2', 'Feature 3')");
+        }
+    }
+}
