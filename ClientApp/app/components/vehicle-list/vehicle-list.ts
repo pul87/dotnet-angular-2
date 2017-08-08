3b35c539fdcb221a02b2f7adbc5be886f0009391
Paolo Basso <paolo.basso87@gmail.com>
2017-08-08 08:24:37 +0200

    Aggiornata gestione filtri client con logica filtraggio lato server

--- ClientApp/app/components/vehicle-list/vehicle-list.ts
+++ ClientApp/app/components/vehicle-list/vehicle-list.ts
@@ -8,7 +8,6 @@ import { Component, OnInit } from '@angular/core';
 export class VehicleListComponent implements OnInit {
     
     vehicles: Vehicle[];
-    allVehicles: Vehicle[];
     makes: KeyValuePair[];
     filter: any = {};
 
@@ -19,21 +18,16 @@ export class VehicleListComponent implements OnInit {
         this.vehicleService.getMakes()
         .subscribe( makes => this.makes = makes);
 
-        this.vehicleService.getVehicles()
-        .subscribe( vehicles => this.vehicles = this.allVehicles = vehicles);
+        this.populateVehicles();
     }
 
     onFilterChange() {
+        this.populateVehicles();
+    }
 
-        var vehicles = this.allVehicles;
-
-        if (this.filter.makeId)
-            vehicles = vehicles.filter( v => v.make.id == this.filter.makeId );
-
-        if (this.filter.modelId)
-            vehicles = vehicles.filter( v => v.model.id == this.filter.modelId );
-
-        this.vehicles = vehicles;
+    private populateVehicles() {
+        this.vehicleService.getVehicles(this.filter)
+        .subscribe( vehicles => this.vehicles = vehicles);
     }
 
     resetFilter() {
