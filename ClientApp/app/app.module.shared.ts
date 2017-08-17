9cc3445e77f29ae4e426860449da6babe79d82f3
Paolo Basso <paolo.basso87@gmail.com>
2017-08-17 17:00:51 +0200

    Chart.js

--- ClientApp/app/app.module.shared.ts
+++ ClientApp/app/app.module.shared.ts
@@ -4,6 +4,7 @@ import { AdminComponent } from './components/admin/admin.component';
 import { AuthService } from './services/auth.service';
 import { BrowserXhr } from '@angular/http';
 import { BrowserXhrWithProgress, ProgressService } from './services/progress.service';
+import { ChartModule } from 'angular2-chartjs';
 import { PhotoService } from './services/photo.service';
 import { VehicleListComponent } from './components/vehicle-list/vehicle-list';
 import * as Raven from 'raven-js';
@@ -23,11 +24,11 @@ import { CounterComponent } from './components/counter/counter.component';
 import { PaginationComponent } from './components/shared/pagination-component/pagination.component';
 import { ViewVehicleComponent } from "./components/view-vehicle/view-vehicle";
 import { AUTH_PROVIDERS } from "angular2-jwt/angular2-jwt";
-
+/*
 Raven
   .config('https://b7a31838e56b497fb10075045f0a3b3e@sentry.io/200090')
   .install();
-
+*/
 export const sharedConfig: NgModule = {
     bootstrap: [ AppComponent ],
     declarations: [
@@ -45,14 +46,15 @@ export const sharedConfig: NgModule = {
     imports: [
         FormsModule,
         ToastyModule.forRoot(),
+        ChartModule,
         RouterModule.forRoot([
             { path: '', redirectTo: 'vehicles', pathMatch: 'full' },
             { path: 'home', component: HomeComponent },
             { path: 'admin', component: AdminComponent, canActivate: [ AdminAuthGuard ] },
             { path: 'fetch-data', component: FetchDataComponent },
             { path: 'vehicles', component: VehicleListComponent },
-            { path: 'vehicles/new', component: VehicleFormComponent },
-            { path: 'vehicles/edit/:id', component: VehicleFormComponent },
+            { path: 'vehicles/new', component: VehicleFormComponent, canActivate: [ AuthGuard ] },
+            { path: 'vehicles/edit/:id', component: VehicleFormComponent, canActivate: [ AuthGuard ] },
             { path: 'vehicles/:id', component: ViewVehicleComponent },
             { path: '**', redirectTo: 'home' }
         ])
